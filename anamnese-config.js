663f915c3ba3a00a5256c50355df1786f96dc768
ricmurtapsicologia <164411318+ricmurtapsicologia@users.noreply.github.com>
2026-08-09 23:38:38 -0300

    chore: adiciona configuracao isolada da ponte de anamnese

---
+++ anamnese-config.js
@@ -0,0 +1,8 @@
+/*
+ * Configuração da anamnese nativa.
+ * Preencha ANAMNESE_BRIDGE_URL com a URL /exec do Web App do Apps Script
+ * depois de publicar apps-script/AnamneseBridge.gs.
+ *
+ * Não coloque segredos neste arquivo: ele é público no GitHub Pages.
+ */
+window.ANAMNESE_BRIDGE_URL = '';
